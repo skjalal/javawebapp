6c1beddfdf01a295c9d27ed31fffeb42e8062868
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2020-03-08 19:01:34 +0530

    Added Log4J2 dependency

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.example.controller;
 
 import com.example.model.Person;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    private static final Logger LOGGER = Logger.getLogger(HomeController.class);
+    private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
 
     @GetMapping("/")
     public String index(Model model) {
